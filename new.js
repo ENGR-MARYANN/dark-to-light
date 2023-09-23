833f39da247826e8c971d792b0e67c9c20a53487
ENGR-MARYANN <145807862+ENGR-MARYANN@users.noreply.github.com>
2023-09-23 13:05:53 +0100

    Add files via upload

---
+++ new.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+const myDiv = document.getElementById("myDiv");
+
+btn.addEventListener("click",()=>{
+    // class lists-add,remove,toggle
+    myDiv.classList.toggle("dark");
+})
+     
